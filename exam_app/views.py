5a071c6ab6580be4e80033486e7c30c0423a1bb5
dhriti-srivastava <dhritisrivastava.9new@gmail.com>
2026-02-18 00:53:03 +0530

    Implemented random question generation and exam page

--- exam_app/views.py
+++ exam_app/views.py
@@ -1,3 +1,11 @@
+import random
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+def start_exam(request):
+    questions = list(Question.objects.all())
+    
+    # Select 5 random questions
+    random_questions = random.sample(questions, min(len(questions), 5))
+    
+    return render(request, 'exam.html', {'questions': random_questions})
