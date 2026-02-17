36b110a021d6853b538bd197a452b13d98ee9756
dhriti-srivastava <dhritisrivastava.9new@gmail.com>
2026-02-18 00:41:18 +0530

    Added Question model and integrated with admin panel

--- exam_app/models.py
+++ exam_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.TextField()
+    option1 = models.CharField(max_length=255)
+    option2 = models.CharField(max_length=255)
+    option3 = models.CharField(max_length=255)
+    option4 = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question_text
