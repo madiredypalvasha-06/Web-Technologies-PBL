42751945fc9793642dab697717389f499eb36432
Vyshnavi-Sambu4 <s.vyshnavi.6304@gmail.com>
2026-02-18 10:40:45 +0530

    Implemented answer checking and result calculation

--- exam_app/views.py
+++ exam_app/views.py
@@ -4,8 +4,20 @@ from .models import Question
 
 def start_exam(request):
     questions = list(Question.objects.all())
-    
-    # Select 5 random questions
     random_questions = random.sample(questions, min(len(questions), 5))
-    
+
+    if request.method == 'POST':
+        score = 0
+        
+        for question in random_questions:
+            selected_answer = request.POST.get(str(question.id))
+            
+            if selected_answer == question.correct_answer:
+                score += 1
+
+        return render(request, 'result.html', {
+            'score': score,
+            'total': len(random_questions)
+        })
+
     return render(request, 'exam.html', {'questions': random_questions})
