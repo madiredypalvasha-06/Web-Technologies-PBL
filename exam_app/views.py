4a68ce12ee2e160312a26f3ee215f7b1bb8b9991
dhriti-srivastava <dhritisrivastava.9new@gmail.com>
2026-02-18 11:06:48 +0530

    Fixed scoring issue by storing question IDs in session

--- exam_app/views.py
+++ exam_app/views.py
@@ -3,21 +3,29 @@ from django.shortcuts import render
 from .models import Question
 
 def start_exam(request):
-    questions = list(Question.objects.all())
-    random_questions = random.sample(questions, min(len(questions), 5))
 
     if request.method == 'POST':
         score = 0
-        
-        for question in random_questions:
+        question_ids = request.session.get('exam_questions', [])
+
+        questions = Question.objects.filter(id__in=question_ids)
+
+        for question in questions:
             selected_answer = request.POST.get(str(question.id))
-            
             if selected_answer == question.correct_answer:
                 score += 1
 
         return render(request, 'result.html', {
             'score': score,
-            'total': len(random_questions)
+            'total': len(questions)
         })
 
-    return render(request, 'exam.html', {'questions': random_questions})
+    else:
+        questions = list(Question.objects.all())
+        random_questions = random.sample(questions, min(len(questions), 5))
+
+        # Store question IDs in session
+        request.session['exam_questions'] = [q.id for q in random_questions]
+
+        return render(request, 'exam.html', {'questions': random_questions})
+
